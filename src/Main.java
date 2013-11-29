3d770a789d09fef4a46c7d5081d57903482eaa5a
Santiago Paiva <santiago.paiva@mail.mcgill.ca>
2013-11-28 22:28:50 -0500

    Second commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author Santiago Paiva
+ * 
+ */
+
+public class Main {
+    public static void main(String[] args) throws ParserConfigurationException, 
+            FileNotFoundException, SAXException, IOException
+    {
+        
+        XMLGenerator writeAntXML = new XMLGenerator();
+        XMLReader readAntXML = new XMLReader();
+        XMLModifier sim = new XMLModifier();
+        
+        readAntXML.readAntBody();
+        //writeAntXML.XMLGenerator();
+        
+    }   
+}

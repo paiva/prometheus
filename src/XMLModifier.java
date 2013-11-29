0f0b07a39d3984715f1e172d1b1d21fd14743537
Santiago Paiva <santiago.paiva@mail.mcgill.ca>
2013-11-28 22:49:11 -0500

    Adding XMLModifier.java

---
+++ src/XMLModifier.java
@@ -0,0 +1,23 @@
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.xml.parsers.ParserConfigurationException;
+
+
+public class XMLModifier {
+       
+    ArrayList trueValsArray = new ArrayList();
+    
+    public void generateTrueVals(ArrayList<String> gene, ArrayList<String> expression)
+    {
+        System.out.println("Gene Array " + gene);
+        System.out.println("Expression Array " + expression);
+        
+        //Need to work on IF statments to fill trueValsArray
+        
+        //Need to generate a specific XML file with new input data
+        
+    }
+    
+}

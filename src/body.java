77ca18a594e1c3edaf089d881fc5fef7cc2c821f
Santiago Paiva <santiago.paiva@mail.mcgill.ca>
2013-12-29 16:35:52 -0500

    Added backbone methods in body.java

--- src/body.java
+++ src/body.java
@@ -1,10 +1,44 @@
-
-
-
 /**
- *
- * @author Santiago
+ * 
+ * @author Santiago Paiva
+ * @version 1.0
  */
+
 public class body {
+ 
+    public static move(up, down, left, right, speed){
+            
+        return; 
+    }
+        
+    public static releasePheromone(type){
+        
+        return; 
+    }
     
+    public static pickUp(strength){
+        
+        return; 
+    }
+    
+    public static eat(){
+        
+        return; 
+    }
+    
+    public static bite(){
+        
+        return;
+    }
+    
+    public static smell(left, right, front){
+        
+        return ; 
+    }
+    
+    public static touch(left, right, front){
+    
+        return; 
+    }
+    }
 }
